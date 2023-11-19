110d669485bf667035b5c73b95a34bdc7812aa6e
Benzazoua <billalben252@gmail.com>
2023-11-19 15:14:15 +0100

    Prepare The Project

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+/*=============== SHOW MENU ===============*/
+
+/*===== Menu Show =====*/
+/* Validate if constant exists */
+
+/*===== Hide Show =====*/
+/* Validate if constant exists */
+
+/*=============== IMAGE GALLERY ===============*/
+
+/*=============== SWIPER CATEGORIES ===============*/
+
+/*=============== SWIPER PRODUCTS ===============*/
+
+/*=============== PRODUCTS TABS ===============*/
