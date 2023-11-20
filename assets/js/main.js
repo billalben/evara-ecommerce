02ebbe2ace501539705c7d2b2cc64253a273ae6d
Benzazoua <billalben252@gmail.com>
2023-11-20 17:53:40 +0100

    Create Categories Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,30 @@
 
 /*=============== SWIPER CATEGORIES ===============*/
 
+let swiperCategories = new Swiper(".categories__container", {
+  spaceBetween: 24,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1400: {
+      slidesPerView: 6,
+      spaceBetween: 24,
+    },
+  },
+});
+
 /*=============== SWIPER PRODUCTS ===============*/
 
 /*=============== PRODUCTS TABS ===============*/
