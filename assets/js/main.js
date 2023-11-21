1e0d623accf95d7e09679232d7f58e88888c950b
Benzazoua <billalben252@gmail.com>
2023-11-21 21:52:32 +0100

    Products Tabs JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,4 +38,22 @@ let swiperCategories = new Swiper(".categories__container", {
 
 /*=============== PRODUCTS TABS ===============*/
 const tabs = document.querySelectorAll("[data-target]"),
-  tabsContent = document.querySelectorAll("[content]");
+  tabsContents = document.querySelectorAll("[content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabsContents.forEach((tabsContent) => {
+      tabsContent.classList.remove("active-tab");
+    });
+
+    target.classList.add("active-tab");
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("active-tab");
+    });
+
+    tab.classList.add("active-tab");
+  });
+});
