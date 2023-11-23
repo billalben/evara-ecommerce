30054038d32b611fb9238bf64c594d17ebf3a3c3
Benzazoua <billalben252@gmail.com>
2023-11-23 13:59:04 +0100

    Create New Arrivals Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,6 +35,29 @@ let swiperCategories = new Swiper(".categories__container", {
 });
 
 /*=============== SWIPER PRODUCTS ===============*/
+let swiperProducts = new Swiper(".new__container", {
+  spaceBetween: 24,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1400: {
+      slidesPerView: 4,
+      spaceBetween: 24,
+    },
+  },
+});
 
 /*=============== PRODUCTS TABS ===============*/
 const tabs = document.querySelectorAll("[data-target]"),
