48a6c705a68aec677d043ce8fefd59475b62893d
Benzazoua <billalben252@gmail.com>
2023-11-29 19:31:56 +0100

    Response Design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,34 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*===== Menu Show =====*/
 /* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
 /*===== Hide Show =====*/
 /* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== IMAGE GALLERY ===============*/
 function imgGallery() {
   const mainImg = document.querySelector(".details__img"),
     smallImg = document.querySelectorAll(".details__small-img");
 
-    smallImg.forEach((img) => {
-      img.addEventListener("click", function () {
-        mainImg.src = this.src;
-      })
-    })
+  smallImg.forEach((img) => {
+    img.addEventListener("click", function () {
+      mainImg.src = this.src;
+    });
+  });
 }
 
 imgGallery();
@@ -30,13 +43,21 @@ let swiperCategories = new Swiper(".categories__container", {
   },
 
   breakpoints: {
-    640: {
+    350: {
       slidesPerView: 2,
-      spaceBetween: 20,
+      spaceBetween: 24,
     },
     768: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+    992: {
       slidesPerView: 4,
-      spaceBetween: 40,
+      spaceBetween: 24,
+    },
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 24,
     },
     1400: {
       slidesPerView: 6,
@@ -55,13 +76,13 @@ let swiperProducts = new Swiper(".new__container", {
   },
 
   breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
     768: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    992: {
       slidesPerView: 4,
-      spaceBetween: 40,
+      spaceBetween: 24,
     },
     1400: {
       slidesPerView: 4,
